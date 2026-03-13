5834766932e9771328ad024ca7641a6d142583e7
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2026-03-13 14:00:51 +0500

    Added Delete and update action from products

--- src/app/admin/products/actions.ts
+++ src/app/admin/products/actions.ts
@@ -3,6 +3,7 @@
 import { db } from "@/db";
 import { products } from "@/db/schema";
 import { revalidatePath } from "next/cache";
+import { eq } from "drizzle-orm";
 
 export async function createProduct(data: {
     name: string;
@@ -34,5 +35,49 @@ export async function createProduct(data: {
     } catch (error) {
         console.error("Database Error:", error);
         return { success: false, error: "Failed to create product." };
+    }
+}
+
+export async function deleteProduct(id: number) {
+    try {
+        await db.delete(products).where(eq(products.id, id));
+        revalidatePath("/admin/products");
+        return { success: true };
+    } catch (error) {
+        console.error("Failed to delete product:", error);
+        return { success: false, error: "Failed to delete product." };
+    }
+}
+
+export async function updateProduct(id: number, data: {
+    name: string;
+    categoryId: number;
+    description: string;
+    price: string;
+    stockQuantity: number;
+    imageUrl: string;
+    isNew: boolean;
+    isOnSale: boolean;
+}) {
+    const slug = data.name.toLowerCase().replace(/[^a-z0-9]+/g, '-').replace(/(^-|-$)+/g, '');
+
+    try {
+        await db.update(products).set({
+            name: data.name,
+            slug,
+            categoryId: data.categoryId,
+            description: data.description,
+            price: data.price,
+            stockQuantity: data.stockQuantity,
+            imageUrl: data.imageUrl,
+            isNew: data.isNew,
+            isOnSale: data.isOnSale,
+        }).where(eq(products.id, id));
+
+        revalidatePath("/admin/products");
+        return { success: true };
+    } catch (error) {
+        console.error("Failed to update product:", error);
+        return { success: false, error: "Failed to update product." };
     }
 }
